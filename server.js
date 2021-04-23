6e29a401253e729655d2ae369c82daef3ab5a508
smartful <remi_rodrigues@hotmail.com>
2021-04-23 09:07:48 +0200

    API routes creation

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper API !'}));
 
+// Define Routes
+app.use('/api/users/', require('./routes/users'));
+app.use('/api/contacts/', require('./routes/contacts'));
+app.use('/api/auth/', require('./routes/auth'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

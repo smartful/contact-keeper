e7a4096f6bc728bab710b8ff28a866f0628ab9fc
smartful <remi_rodrigues@hotmail.com>
2021-04-25 00:56:58 +0200

    implementation of json web token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
+
+const User = require('../models/Users');
 
 // @route   GET api/auth
 // @desc    Get logged in user
@@ -11,8 +17,46 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    Auth user and get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post('/', 
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid credentials" });
+      }
+
+      const isMatch = bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid credentials" });
+      }
+
+      const payload = {
+        user : {
+          id: user.id,
+        }
+      };
+
+      jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (error, token) => {
+          if (error) throw error;
+          res.json({ token });
+      });
+    } catch (error) {
+      console.error(`Error : ${error.message}`);
+      res.status(500).send("Server Error");
+    }
+
+  }
+);
 
 module.exports = router;

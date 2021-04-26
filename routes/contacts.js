64c8c9151ab954cf9ddbcb2020fb381746491333
smartful <remi_rodrigues@hotmail.com>
2021-04-26 02:16:30 +0200

    add contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -22,9 +22,35 @@ router.get('/', auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post('/', [
+    auth,
+    [
+      check('name', 'Name is required').not().isEmpty(),
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (error) {
+      console.error(`Error : ${error.message}`);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact

10625e27f22883094d98242bbcf576598ec46983
smartful <remi_rodrigues@hotmail.com>
2021-04-28 02:56:12 +0200

    creation of global react layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Contact Keeper</h1>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 

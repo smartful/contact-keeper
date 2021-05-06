cef8764e245462b652462d40c03ed85ae069520f
smartful <remi_rodrigues@hotmail.com>
2021-05-06 03:15:33 +0200

    update contact to bdd

--- client/src/context/contact/ContactReducer.js
+++ client/src/context/contact/ContactReducer.js
@@ -29,7 +29,7 @@ const ContactReducer = (state, action) => {
       return {
         ...state,
         contacts: state.contacts.map(contact => (
-          contact.id === action.payload.id ? action.payload : contact
+          contact._id === action.payload._id ? action.payload : contact
         )),
         loading: false,
       }
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -62,7 +62,21 @@ const ContactState = ({ children }) => {
 
   const clearCurrent = () => dispatch({ type: CLEAR_CURRENT });
 
-  const updateContact = (contact) => dispatch({ type: UPDATE_CONTACT, payload: contact });
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg});
+    }
+
+  }
 
   const filterContacts = (text) => dispatch({ type: FILTER_CONTACTS, payload: text });
 
